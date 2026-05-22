2f3c7e73537d9f0aaf694b84f2e36e0cf8b219bb
Mahdi Al Hasan <webpro.mahdi@gmail.com>
2026-05-22 11:39:56 +0600

    feat(middleware): add auth middleware for JWT verification and role-based access control

--- src/middleware/auth.ts
+++ src/middleware/auth.ts
@@ -1,9 +1,9 @@
 import type { NextFunction, Request, Response } from "express";
 import jwt, { type JwtPayload } from "jsonwebtoken";
 import config from "../config/index.js";
-import { pool } from "../db/index.js";
+import type { UserRole } from "../module/auth/auth.interface.js";
 
-const auth = () => {
+const auth = (...roles: UserRole[]) => {
   return async (req: Request, res: Response, next: NextFunction) => {
     try {
       const token = req.headers.authorization;
@@ -16,19 +16,19 @@ const auth = () => {
       }
 
       const decoded = jwt.verify(token, config.jwtSecret) as JwtPayload;
-      const userData = await pool.query(
-        `SELECT * FROM users WHERE email = $1`,
-        [decoded.email],
-      );
-      const user = userData.rows[0];
-      if (userData.rows.length === 0) {
-        res.status(404).json({
+      req.user = decoded;
+      if (roles.length && !roles.includes(req.user.role)) {
+        return res.status(403).json({
           success: false,
-          message: "User not found",
+          message: "User is not authorized",
         });
       }
+
+      next();
     } catch (error) {
       next(error);
     }
   };
 };
+
+export default auth;
--- src/module/issues/issues.route.ts
+++ src/module/issues/issues.route.ts
@@ -1,8 +1,9 @@
 import { Router } from "express";
 import { issueController } from "./issues.controller.js";
+import auth from "../../middleware/auth.js";
 
 const router = Router();
 
-router.post("/", issueController.createIssue);
+router.post("/", auth(), issueController.createIssue);
 
 export const issuesRouter = router;
