4c1c4717669e60bbc00757a6caa43fde9b3794f2
Mahdi Al Hasan <webpro.mahdi@gmail.com>
2026-05-22 12:39:34 +0600

    feat(issues): authenticated users can now create bug reports and feature requests

--- src/module/issues/issues.controller.ts
+++ src/module/issues/issues.controller.ts
@@ -1,4 +1,17 @@
-const createIssue = () => {};
+import type { Request, Response } from "express";
+import catchAsync from "../../utility/catchAsync.js";
+import sendResponse from "../../utility/sendResponse.js";
+import { issuesService } from "./issues.service.js";
+
+const createIssue = catchAsync(async (req: Request, res: Response) => {
+  const result = await issuesService.createIssueIntoDB(req.body, req.user!.id);
+  sendResponse(res, {
+    statusCode: 201,
+    success: true,
+    message: "Issue created successfully",
+    data: result.rows[0],
+  });
+});
 
 export const issueController = {
   createIssue,
--- src/module/issues/issues.service.ts
+++ src/module/issues/issues.service.ts
@@ -1,4 +1,19 @@
-const createIssueIntoDB = () => {};
+import { pool } from "../../db/index.js";
+import type { IIssue } from "./issues.interface.js";
+
+const createIssueIntoDB = async (payload: IIssue, reporterId: number) => {
+  const { title, description, type } = payload;
+
+  const result = await pool.query(
+    `
+      INSERT INTO issues(title, description, type, reporter_id)
+      VALUES($1, $2, $3, $4)
+      RETURNING *;
+     `,
+    [title, description, type, reporterId],
+  );
+  return result;
+};
 
 export const issuesService = {
   createIssueIntoDB,
